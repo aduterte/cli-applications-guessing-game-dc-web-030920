84b98b08238c1ef7d5c595a2f0609d99fa40480e
Alessandro Duterte <alexduterte@gmail.com>
2020-02-02 20:20:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
   number = rand(1...6)
   puts "Guess a number between 1 and 6"
   input = gets.chomp
-  binding.pry
+  puts input
   if input == number
     puts "You guessed the correct number!"
   elsif input == "exit"
